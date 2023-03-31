33449b2fba3bbb47ad137f266d44a325366b85aa
VenchasS <veniy.ru@yandex.ru>
2023-04-01 01:33:36 +0500

    Venchass
    test store

---
+++ src/pages/post.tsx
@@ -0,0 +1,7 @@
+export const Post = () => {
+  return (
+    <>
+      Post
+    </>
+  );
+};
---
+++ src/pages/venchTest.tsx
@@ -0,0 +1,18 @@
+import { increment } from "../store/reducers/test";
+import { store, useAppDispatch } from "../store/store";
+
+export const VenchTest = () => {
+  const dispatch = useAppDispatch();
+  const val = store.getState().login.value;
+  const addNumber = () => {
+    dispatch(increment());
+    // eslint-disable-next-line no-console
+    console.log(store.getState());
+  };
+  return (
+    <>
+    Login {val}
+      <button onClick={addNumber}>Add Number to store</button>
+    </>
+  );
+};
