92a64cc04ac7aabb9fbdf0c5a6e94e539b7bcbf1
v1ab1 <sharkandmouse@gmail.com>
2023-05-21 00:28:44 +0500

    profile fixes

--- src/Components/Profile/Events/index.tsx
+++ src/Components/Profile/Events/index.tsx
@@ -1,8 +1,9 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
 import { EventCard } from "../../EventCard";
 import { SelectedTab } from "../../../Pages/Profile";
-import styles from "./Events.module.sass";
 import { Ipost } from "../../../API/post";
+import styles from "./Events.module.sass";
 
 interface EventsInterface {
   selected: SelectedTab,
@@ -16,8 +17,11 @@ const EventsEmpty = () => {
     <div className={styles.EventsEmpty}>
       <img src={require("../../../assets/profile/Yn6hmt4S69A.jpg")} alt="здесь пусто" width={328}/>
       <p className={styles.EmptyEventsText}>
-        Ой, тут пусто.
-        Создайте мероприятие нажав на соответствующую кнопку в верхней части профиля.
+        Ой, тут пусто.<br />
+        <Link to="/events/create" className={styles.link}>
+          Создайте мероприятие
+        </Link>
+        &nbsp;нажав на соответствующую кнопку в верхней части профиля.
       </p>
     </div>
   );
