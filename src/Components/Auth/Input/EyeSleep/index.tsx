9a295f22983de73f871f9e6f6538ca2c2232802a
v1ab1 <sharkandmouse@gmail.com>
2023-07-25 16:07:34 +0500

    show-pass-btn\

---
+++ src/Components/Auth/Input/EyeSleep/index.tsx
@@ -0,0 +1,19 @@
+export const EyeSleep = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="800"
+      height="800"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#000"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M2.999 3l18 18M9.843 9.914a3 3 0 004.265 4.22M6.5 6.646A10.024 10.024 0 002.457 12c1.274 4.057 5.065 7 9.542 7 1.99 0 3.842-.58 5.4-1.582m-6.4-12.369c.329-.032.663-.049 1-.049 4.478 0 8.268 2.943 9.542 7a9.954 9.954 0 01-1.189 2.5"
+      ></path>
+    </svg>
+  );
+};
